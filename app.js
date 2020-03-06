49d724fdb6151e99adc734b3dc1b3d9218f45987
Nic Mays <Nicmays334@gmail.com>
2020-03-06 10:34:46 -0500

    Skeleton Built

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var homework1Router = require('./routes/homework1');
+var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('public'));
 
-app.use('/', homework1Router);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
